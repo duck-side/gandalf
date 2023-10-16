a30de70a98e011480ec1f23257ddad3b8eb9dc8d
Dimitri Reifschneider <dimitri.reifschneider@gmail.com>
2023-10-16 18:44:57 +0200

    fix: removes echo

--- gandalf.sh
+++ gandalf.sh
@@ -6,21 +6,17 @@ TIME=$(date -u +"%-H")
 DAY=$(date -u +"%u")
 
 if [[ $RESPOND == 'true' ]]; then
-  echo "🧙🏻‍♂️ You shall pass"
   echo "true"
   exit 0;
 fi
 
 if [[ $RESPOND == 'false' ]]; then
-  echo "🧙🏻‍♂️ You shall not pass"
   echo "false"
   exit 0;
 fi
 
 if [[ ($TIME -ge $START_HOURS && $TIME -lt $END_HOURS) && ($DAY -ge $START_DAY && $DAY -le $END_DAY) ]]; then
-  echo "🧙🏻‍♂️ You shall pass"
   echo "true"
 else
-  echo "🧙🏻‍♂️ You shall not pass"
   echo "false"
 fi
